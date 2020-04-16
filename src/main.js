b6a0b1980f5b217115c3550a13db7c45dd738493
Joshua Hellman <jhell85@gmail.com>
2020-04-16 11:02:29 -0700

    initial commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import $ from "jquery";
+
+export class Player{
+  contructor(){
+    this.health = 0;
+    this.weapon = {name:"Sword",
+    damage: 20
+  }}
+}
