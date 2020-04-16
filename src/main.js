ff341dc4aa0d032694f8264f2f16446bb54695a1
Joshua Hellman <jhell85@gmail.com>
2020-04-16 11:29:31 -0700

    add classes for player and enemies.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,38 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './styles.css';
 import $ from "jquery";
 
 export class Player{
-  contructor(){
-    this.health = 0;
-    this.weapon = {name:"Sword",
+  constructor(player){
+    this.player = player;
+    this.health = 100;
+    this.weapon = {
+    name:"Sword",
     damage: 20
-  }}
+    }
+  }
 }
+// human, chimeara, minotaur 
+export class Enemy {
+  constructor(type) {
+    this.enemyType = type;
+    this.enemyHealth = 0;
+    this.damageOutout = 0;
+  } 
+  populateEnemy(){
+    if(this.enemyType === 'human'){
+      this.enemyHealth = 50;
+      this.damageOutout = 20;
+    }else if( this.enemyType === 'chimera'){
+      this.enemyHealth = 100;
+      this.damageOutout = 25; 
+    }else if( this.enemyType === 'minotaur'){
+      this.enemyHealth = 150;
+      this.damageOutout = 15; 
+    }
+    // add stats based on type
+  }
+}
+
+
